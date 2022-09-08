c9388db5917c434914005c0c0c34c49eec11f972
Ritu Kumar <95040772+Rupakrajput19@users.noreply.github.com>
2022-09-08 15:06:35 +0530

    Delete src directory

--- src/Components/About/about_style.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles({
-  about_heading: {
-    color: "white",
-    textAlign: "center",
-    margin: "100px auto",
-  }
-});
--- src/Components/Contact/contact_style.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-export default makeStyles({
-  contact_heading: {
-    color: "white",
-    textAlign: "center",
-    margin: "100px auto",
-  }
-});
